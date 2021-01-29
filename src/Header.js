ba1b7a32358a86bb84c9799879c1df27e5606dd6
olefirenkoe <olefirenkoevgen@gmail.com>
2021-01-29 22:42:00 +0200

    modified

--- src/Header.js
+++ src/Header.js
@@ -19,23 +19,12 @@ class Header extends React.Component{
 
 export default Header;
 
-function auth(data) {
-    window.SE.authenticate({
-        success: function(data) { alert('Я получил доступ!'); }, // Приложение авторизовало пользователя
-        error: function(data) {  alert('Я не получил доступ :('); }, // Приложение не авторизовало пользователя
-    });
-}
-
-window.SE.init({
+SE.init({
 	clientId: 19555, // Здесь мы ставим выданный нам clientId
 	key: 'qBt3pH)yY2*kx96ogUORkA((', // А здесь соответственно key
-	channelUrl: 'https://pages.github.com/', // Особое внимание стоит уделить этому полю. Здесь нужно указать домен, на котором хостится и крутится приложение
-	complete: auth() // Здесь мы указываем некоторую функцию, которая будет выполнена в случае успеха
+	channelUrl: 'http://example.com/', // Особое внимание стоит уделить этому полю. Здесь нужно указать домен, на котором хостится и крутится приложение
+	complete: function (data) { alert("Я загрузился!"); } // Здесь мы указываем некоторую функцию, которая будет выполнена в случае успеха
 });
-
-
-
-alert(2);
 
 
 // 19555 id
