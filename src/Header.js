dbd54b98b4a1b4a8580037dbf17a404f60b33308
olefirenkoe <olefirenkoevgen@gmail.com>
2021-01-29 13:32:23 +0200

    add stack script

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from '../src/logo.png';
 import '../src/header.css';
+// import SE from 'https://cdn.sstatic.net/apiv2/js/all.js?v=bc9137ab1d55';
 
+ 
 class Header extends React.Component{
     render(){
         return(
@@ -9,9 +11,27 @@ class Header extends React.Component{
                 <img src={logo} className="logo" alt="logo"/>
                 <span className="label">stack <b>usof</b></span>
                 <button>Log in</button>
+
             </header>
         )
     }
 }
 
 export default Header;
+
+
+
+window.SE.init({
+	clientId: 19555, // Здесь мы ставим выданный нам clientId
+	key: 'qBt3pH)yY2*kx96ogUORkA((', // А здесь соответственно key
+	channelUrl: 'https://olefirenkoe.github.io/', // Особое внимание стоит уделить этому полю. Здесь нужно указать домен, на котором хостится и крутится приложение
+	complete: function (data) { alert("Я загрузился!"); } // Здесь мы указываем некоторую функцию, которая будет выполнена в случае успеха
+});
+
+
+
+
+
+
+// 19555 id
+// qBt3pH)yY2*kx96ogUORkA(( key 
