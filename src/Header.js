91644042ca1c4029af8b05430fde513988c00127
olefirenkoe <olefirenkoevgen@gmail.com>
2021-01-29 17:41:22 +0200

    modified authentication

--- src/Header.js
+++ src/Header.js
@@ -19,13 +19,18 @@ class Header extends React.Component{
 
 export default Header;
 
-
+function auth(data) {
+    window.SE.authenticate({
+        success: function(data) { alert('Я получил доступ!'); }, // Приложение авторизовало пользователя
+        error: function(data) {  alert('Я не получил доступ :('); }, // Приложение не авторизовало пользователя
+    });
+}
 
 window.SE.init({
 	clientId: 19555, // Здесь мы ставим выданный нам clientId
 	key: 'qBt3pH)yY2*kx96ogUORkA((', // А здесь соответственно key
 	channelUrl: 'https://olefirenkoe.github.io/', // Особое внимание стоит уделить этому полю. Здесь нужно указать домен, на котором хостится и крутится приложение
-	complete: function (data) { alert("Я загрузился!"); } // Здесь мы указываем некоторую функцию, которая будет выполнена в случае успеха
+	complete: auth() // Здесь мы указываем некоторую функцию, которая будет выполнена в случае успеха
 });
 
 
